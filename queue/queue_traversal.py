8174347b8664bd284c1cefdd0e288c961aa126f1
vivek <codecrafter67@gmail.com>
2025-12-26 12:42:36 +0530

    implement queue data structure with enqueue, dequeue, peek, size, and print methods

---
+++ queue/queue_traversal.py
@@ -0,0 +1,45 @@
+class Queue:
+    
+    def __init__(self):
+        self.queue = []
+        self.length = 0
+        self.front = -1
+        self.rear = -1
+        
+    def enqueue(self,value):
+        if self.front < 0:
+            self.front+=1
+        self.rear+=1
+        self.length+=1
+        self.queue.append(value)
+        
+    def dequeue(self):
+        if self.front < 0:
+             print("Queue is empty ! ")
+        else:
+            self.front+=1
+            self.length-=1
+            
+    def peek(self):
+        print(self.queue[self.front])
+        
+    def size(self):
+        return self.length
+    
+    def printQueue(self):
+        for i in range(self.length):
+            print(self.queue[i])
+            self.enqueue(self.dequeue())
+            
+        
+    
+    
+    
+q = Queue()
+
+q.enqueue(10)
+q.enqueue(20)
+q.enqueue(30)
+     
+
+q.printQueue()
