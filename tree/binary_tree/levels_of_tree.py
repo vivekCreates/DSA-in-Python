a29bff46a525c9fd0dfc6097f4550abb1fbac9b3
vivek <codecrafter67@gmail.com>
2025-12-28 20:24:23 +0530

    feat: add level calculation function for binary tree

---
+++ tree/binary_tree/levels_of_tree.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self,value):
+        self.data = value
+        self.left = self.right = None
+        
+        
+a = Node(3)
+b = Node(2)
+c = Node(4)
+d = Node(1)
+e = Node(5)
+f = Node(6)
+g = Node(9)
+
+a.left = b
+a.right = c
+c.left = f
+c.right = g
+b.left = d
+b.right = e
+
+
+def level(root):
+    if root == None:
+        return 0
+    return 1 + max(level(root.left),level(root.right))
+    
+    
+    
+print("level: ",level(a))
