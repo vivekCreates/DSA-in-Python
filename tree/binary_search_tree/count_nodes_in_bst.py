8004779c715d6b436448cace9290ef2656007d6b
vivek <codecrafter67@gmail.com>
2025-12-30 16:26:21 +0530

    feat: add function to count nodes within a specified range in binary search tree

---
+++ tree/binary_search_tree/count_nodes_in_bst.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self,value):
+        self.data = value
+        self.left = self.right = None
+        
+        
+
+a = Node(10)
+b = Node(5)
+c = Node(50)
+d = Node(1)
+e = Node(40)
+f = Node(100)
+
+
+a.left = b
+a.right = c
+b.left = d
+c.left= e
+c.right = f
+
+
+def count_nodes(root, l, h):
+    if root is None:
+        return 0
+    
+    if l <= root.data <= h:
+        return 1 + count_nodes(root.left, l, h) + count_nodes(root.right, l, h)
+    else:
+        return count_nodes(root.left, l, h) + count_nodes(root.right, l, h)
+
+
+print("count: ",count_nodes(a,4,100))
