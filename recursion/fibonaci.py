19d6feb404f70d0cbdc80e3e3821060c7bb01488
vivek <codecrafter67@gmail.com>
2026-01-16 19:38:36 +0530

    implement fibonacci using recursion

---
+++ recursion/fibonaci.py
@@ -0,0 +1,14 @@
+import time
+
+def fibo(n):
+    if n <=1:
+        return n
+    return fibo(n-1)+fibo(n-2)
+
+
+start = time.time()
+ans = fibo(50)
+print(ans)
+end = time.time()
+
+print("time: ",end-start)
