119b8ccf4eb1fdcb6bc2e82ffe8197f2ce268214
vivek <codecrafter67@gmail.com>
2026-01-17 17:53:25 +0530

    optimise path sum in matrix problem with dp

--- dynamic-programming/path_sum_in_matrix.py
+++ dynamic-programming/path_sum_in_matrix.py
@@ -1,4 +1,4 @@
-def paths(arr, r, c):
+def paths(arr, r, c,dp):
     n = len(arr)
     m = len(arr[0])
 
@@ -7,16 +7,22 @@ def paths(arr, r, c):
 
     if r == n - 1:
         return arr[r][c]
+    
+    if dp[r][c]!=-1: return dp[r][c]
 
-    left = paths(arr, r + 1, c - 1)
-    down = paths(arr, r + 1, c)
-    right = paths(arr, r + 1, c + 1)
-
-    return arr[r][c] + max(left, down, right)
+    left = paths(arr, r + 1, c - 1,dp)
+    down = paths(arr, r + 1, c,dp)
+    right = paths(arr, r + 1, c + 1,dp)
+    dp[r][c] = arr[r][c] + max(left, down, right)
+    return dp[r][c]
 
 
 mat = [[2, 7],
        [9, 15]]
 
-ans = max(paths(mat, 0, c) for c in range(len(mat[0])))
+def path_sum(mat,m,n):
+    dp = [[-1 for _ in range(n)] for _ in range(m)]
+    return max(paths(mat, 0, c,dp) for c in range(len(mat[0])))
+
+ans = path_sum(mat,len(mat),len(mat[0]))
 print("ans:", ans)
