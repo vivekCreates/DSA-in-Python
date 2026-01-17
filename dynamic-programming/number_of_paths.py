b771cc9445be72d17a774b291c0619c14f313113
vivek <codecrafter67@gmail.com>
2026-01-17 14:32:20 +0530

    optimize the recursion

--- dynamic-programming/number_of_paths.py
+++ dynamic-programming/number_of_paths.py
@@ -1,13 +1,17 @@
 
 
-def number_of_paths(row,col,m,n):
-    if row==n or col==m:
-        return 1
-    rightways = number_of_paths(row,col+1,m,n)
-    downways = number_of_paths(row+1,col,m,n)
-    return rightways + downways
+def paths(m,n,dp):
+    if m==1 and n==1: return 1
+    if m==0 or n==0: return 0
+    return paths(m-1,n,dp) + paths(m,n-1,dp)
     
     
+
+def number_of_paths(m,n):
+    dp = [-1]* (m*n)
+    print(dp)
+    return paths(m,n,dp)
     
-ans = number_of_paths(1,1,2,3)
+    
+ans = number_of_paths(3,3)
 print("number of ways: ",ans)
