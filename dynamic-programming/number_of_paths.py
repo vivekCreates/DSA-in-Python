a02fbc23a23ebdd482bcf6874b5208a5afbe80d5
vivek <codecrafter67@gmail.com>
2026-01-17 14:50:46 +0530

    optimize with dynamic programming

--- dynamic-programming/number_of_paths.py
+++ dynamic-programming/number_of_paths.py
@@ -1,17 +1,21 @@
-
-
 def paths(m,n,dp):
-    if m==1 and n==1: return 1
-    if m==0 or n==0: return 0
-    return paths(m-1,n,dp) + paths(m,n-1,dp)
+    if m==1 and n==1: 
+        return 1
+    if m==0 or n==0: 
+        return 0
     
+    if dp[m][n]!=-1: 
+        return dp[m][n]
+     
+    dp[m][n] = paths(m,n-1,dp) + paths(m-1,n,dp)
+    return dp[m][n]
     
 
 def number_of_paths(m,n):
-    dp = [-1]* (m*n)
-    print(dp)
+    dp = [[-1 for _ in range(n+1)] for _ in range(m+1)]
     return paths(m,n,dp)
     
     
-ans = number_of_paths(3,3)
+ans = number_of_paths(10,10)
+
 print("number of ways: ",ans)
