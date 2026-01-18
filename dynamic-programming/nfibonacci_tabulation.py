46a904a9608cffe95bfffeb377377805b9e993fa
vivek <codecrafter67@gmail.com>
2026-01-18 15:04:41 +0530

    implement find nfibonacci term using dp with tabulation

---
+++ dynamic-programming/nfibonacci_tabulation.py
@@ -0,0 +1,15 @@
+def nfibonacci(n):
+    if n == 0 or n==1:
+        return n
+    
+    dp = [-1]*(n+1)
+    dp[0] = 0
+    dp[1] = 1
+    
+    for i in range(2,n+1):
+        dp[i] = dp[i-1]+dp[i-2]
+        
+    return dp[n]
+
+
+print("fibo: ",nfibonacci(int(input("Enter the n: "))))
