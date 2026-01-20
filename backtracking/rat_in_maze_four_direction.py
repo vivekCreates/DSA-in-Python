ea4c079cadddaf876cda3e8a88dbb9f3e51b3975
vivek <codecrafter67@gmail.com>
2026-01-20 23:20:04 +0530

    add ways to reached destination function

---
+++ backtracking/rat_in_maze_four_direction.py
@@ -0,0 +1,15 @@
+arr = [
+    [1,1,1,1]
+    [1,1,1,1]
+    [1,1,1,1]
+]
+
+def ways(sr,sc,er,ec,arr):
+    pass
+    
+
+def ways_to_reached_destination(m,n):
+    arr = [[1 for _ in range(n)] for _ in range(m)]
+    return ways(0,0,m,n,arr)
+    
+    
