45b08449fa6abfe3b51b24e49f653df835c7c316
vivek <codecrafter67@gmail.com>
2026-01-21 12:50:51 +0530

    implement the rat in a maze problem with backtracking

--- backtracking/rat_in_maze_four_direction.py
+++ backtracking/rat_in_maze_four_direction.py
@@ -1,15 +1,35 @@
-arr = [
-    [1,1,1,1]
-    [1,1,1,1]
-    [1,1,1,1]
-]
+def ways(sr, sc, er, ec, arr,res):
+    # out of bounds
+    if sr < 0 or sc < 0 or sr > er or sc > ec:
+        return 0
 
-def ways(sr,sc,er,ec,arr):
-    pass
-    
+    # destination reached
+    if sr == er and sc == ec:
+        print(res,end='\n')
+        return 1
 
-def ways_to_reached_destination(m,n):
+    # already visited
+    if arr[sr][sc] == -1:
+        return 0
+
+    # mark visited
+    arr[sr][sc] = -1
+
+    right = ways(sr, sc + 1, er, ec, arr,res+"R")
+    down  = ways(sr + 1, sc, er, ec, arr,res+"D")
+    left  = ways(sr, sc - 1, er, ec, arr,res+"L")
+    up    = ways(sr - 1, sc, er, ec, arr,res+"U")
+
+    # unmark (backtrack)
+    arr[sr][sc] = 1
+
+    return right + down + left + up
+
+
+def ways_to_reached_destination(m, n):
     arr = [[1 for _ in range(n)] for _ in range(m)]
-    return ways(0,0,m,n,arr)
-    
-    
+    return ways(0, 0, m - 1, n - 1, arr,"")
+
+
+ans = ways_to_reached_destination(3, 3)
+print("ways:", ans)
